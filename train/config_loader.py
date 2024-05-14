c761140518468f7f4ddb0ac353bda9e5eeef36dc
Henrique <henrique.tostes@carefy.com.br>
2024-05-14 17:27:10 -0300

    feat: add cofig loader to train pipeline

---
+++ train/config_loader.py
@@ -0,0 +1,19 @@
+import yaml
+import os
+from utils import get_repo_path
+
+
+class ConfigFileLoader:
+
+    def __init__(self, filepath: str = None) -> None:
+        if filepath is None:
+            filepath = os.path.join(get_repo_path(), "config.yaml")
+        if os.path.exists(filepath):
+            self.filepath = filepath
+            with open(self.filepath, "r") as f:
+                self.config = yaml.safe_load(f)
+        else:
+            raise FileNotFoundError(f"Config file not found: {filepath}")
+
+    def get_seed(self) -> int:
+        return self.config["general_config"]["SEED"]
