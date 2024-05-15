d8e8e3fbd310855ae2e9cbd18fbb22f577d44b77
Henrique <henrique.tostes@carefy.com.br>
2024-05-15 11:36:36 -0300

    feat: add new route for models

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,6 @@
-from api.endpoints.v1 import token
+from api.endpoints.v1 import token, models
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(token.router, tags=["token"])
+api_router.include_router(models.router, prefix="/models", tags=["models"])
