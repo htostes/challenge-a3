4c665f95d15085faca059d36c691351855a0a266
Henrique <henrique.tostes@carefy.com.br>
2024-05-15 11:45:24 -0300

    feat: init new schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,5 @@
-from .model import Model
+from .model import Model, ModelIn
 from .token import Token, TokenData
 from .user import User, UserInDB
+from .input import InputIris
+from .output import OutputIris
