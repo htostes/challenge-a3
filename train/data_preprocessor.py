bf73ab163cd37c0e20d09cacf00137dbbf7f7886
Henrique <henrique.tostes@carefy.com.br>
2024-05-14 21:26:15 -0300

    feat: params can be passed through config

--- train/data_preprocessor.py
+++ train/data_preprocessor.py
@@ -1,12 +1,23 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from config_loader import ConfigFileLoader
 
 
-def build_pipeline_rf(n_estimators) -> Pipeline:
+def build_pipeline_rf(
+    params: dict = None, 
+    seed: int = None,
+    config_file_path: str = None,
+) -> Pipeline:
 
+    if seed is None:
+        seed = ConfigFileLoader(config_file_path).get_seed()
+        
+    if params is None:
+        params = ConfigFileLoader(config_file_path).get_params()
+        
     pipeline = Pipeline([
         ('scaler', StandardScaler()),
-        ('random_forest', RandomForestClassifier(n_estimators=n_estimators))
+        ('random_forest', RandomForestClassifier(random_state=seed, **params))
     ])
     return pipeline
