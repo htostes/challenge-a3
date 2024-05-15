714cd235a37b78bcbbd14db4283d14a0b1cc63a8
Henrique <henrique.tostes@carefy.com.br>
2024-05-14 21:42:23 -0300

    docs: remove old docs

--- train/model_trainer.py
+++ train/model_trainer.py
@@ -3,14 +3,15 @@ import mlflow.sklearn
 import sklearn.metrics
 from sklearn.pipeline import Pipeline
 
+
 def train_pipeline(pipeline: Pipeline, X_train, Y_train, X_test, Y_test, experiment_name):
 
     mlflow.set_experiment(experiment_name)
     with mlflow.start_run() as run:
-        # Treinar o modelo
+        # Train the model
         pipeline.fit(X_train, Y_train)
 
-        # Avaliar o modelo
+        # Evaluate the model
         class_list = pipeline.classes_.tolist()
         Y_test_encoded = [class_list.index(y) for y in Y_test]
         Y_predicted = pipeline.predict(X_test)
@@ -22,10 +23,7 @@ def train_pipeline(pipeline: Pipeline, X_train, Y_train, X_test, Y_test, experim
         f1_score = sklearn.metrics.f1_score(Y_test_encoded, Y_predicted_encoded, average='weighted')
         mlflow.log_metric("f1_score", f1_score)
 
-        # Salvar o modelo
-        # mlflow.sklearn.log_model(
-        #     sk_model=pipeline, artifact_path="artifacts", registered_model_name="flower_model"
-        # )
+        # Save the model
         mlflow.sklearn.save_model(
             sk_model=pipeline, path="artifacts",
         )
